b56b1fb3051efa26b78372826ff1738b247d0c63
Isabelle RICHARD <isabelle.richard@smile.fr>
2021-01-25 22:38:38 +0100

    [ADD] Add new feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker is so cool
+          Docker is so cool, new feature
         </p>
         <a
           className="App-link"
